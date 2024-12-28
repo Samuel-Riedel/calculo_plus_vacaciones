8c105c8fdcd99532922781ef8067311d6ba2716a
samuel-riedel <samuel.riedel@outlook.com>
2024-12-28 10:25:33 +0100

    agregue_valores_variables

--- calculo.py
+++ calculo.py
@@ -6,6 +6,7 @@ Sumar dias Festivos Trabajados
 Sumar festivos especiales trabajados
 Sumar Plus de Idiomas en todo el año
 
+Le tengo que dar un valor a cada hora nocturna, domingos trabajados,festivos trabajados y festivos especiales trabajados. estos valores seran en bruto.
 
 Luego esto se suma en un total
 
@@ -13,14 +14,17 @@ al dia
 
 
 """
-
+total_dias_trabajados = 0
 total_horas_nocturnas = 0
 total_domingos_trabajados = 0
 total_dias_festivos_trabajados = 0
 total_dias_festivos_especiales_trabajados = 0
 
+valor_dia_trabajado = 38.24
+valor_hora_nocturna = 1.85
+valor_domingo_trabajado = 14.40
+valor_dia_festivo_trabajado = 41.92
+valor_dias_especial_trabajado = 80.48
 
-valor_hora_nocturna = 0
-valor_domingo_trabajado = 0
-valor_dia_festivo_trabajado = 0
-valor_dias_especial_trabajado = 0
+
+"Hello"
