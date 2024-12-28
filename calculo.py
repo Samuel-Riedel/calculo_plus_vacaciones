e27e84331133e5f1e8e08e813103d6403db84f3b
samuel-riedel <samuel.riedel@outlook.com>
2024-12-28 10:13:03 +0100

    test

---
+++ calculo.py
@@ -0,0 +1,26 @@
+#TODO
+"""
+Sumar todas las horas nocturnas del año que he trabajado
+Tengo que sumar todos los domingos que he trabajado en el año
+Sumar dias Festivos Trabajados
+Sumar festivos especiales trabajados
+Sumar Plus de Idiomas en todo el año
+
+
+Luego esto se suma en un total
+
+al dia
+
+
+"""
+
+total_horas_nocturnas = 0
+total_domingos_trabajados = 0
+total_dias_festivos_trabajados = 0
+total_dias_festivos_especiales_trabajados = 0
+
+
+valor_hora_nocturna = 0
+valor_domingo_trabajado = 0
+valor_dia_festivo_trabajado = 0
+valor_dias_especial_trabajado = 0
